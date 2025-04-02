7f2ae55296f5b2df2deeac1a31b4115fa370426a
Alexey <alyosha.shamaev@mail.ru>
2025-04-02 11:11:42 +0900

    Interface pledge

--- DVD-rent/PledgeList.designer.cs
+++ DVD-rent/PledgeList.designer.cs
@@ -40,16 +40,16 @@ namespace DVD_rent
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(600, 371);
+            this.dataGridView1.Size = new System.Drawing.Size(582, 323);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(618, 342);
+            this.btnAdd.Location = new System.Drawing.Point(438, 337);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(170, 41);
+            this.btnAdd.Size = new System.Drawing.Size(74, 19);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "Добавить";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -57,19 +57,19 @@ namespace DVD_rent
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(618, 296);
+            this.btnExit.Location = new System.Drawing.Point(518, 337);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(170, 40);
+            this.btnExit.Size = new System.Drawing.Size(74, 19);
             this.btnExit.TabIndex = 2;
-            this.btnExit.Text = "Назад";
+            this.btnExit.Text = "Закрыть";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(618, 250);
+            this.button1.Location = new System.Drawing.Point(358, 337);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 40);
+            this.button1.Size = new System.Drawing.Size(74, 19);
             this.button1.TabIndex = 3;
             this.button1.Text = "Удалить";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,9 +77,9 @@ namespace DVD_rent
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 204);
+            this.button2.Location = new System.Drawing.Point(9, 337);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 40);
+            this.button2.Size = new System.Drawing.Size(74, 19);
             this.button2.TabIndex = 4;
             this.button2.Text = "Обновить";
             this.button2.UseVisualStyleBackColor = true;
@@ -89,14 +89,14 @@ namespace DVD_rent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.dataGridView1);
             this.Name = "PledgeList";
-            this.Text = "Form_List";
+            this.Text = "PledgeList";
             this.Load += new System.EventHandler(this.Form_List_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
