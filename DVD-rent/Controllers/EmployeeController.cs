3958b72d67b5c55a19e64709e307bf1b7d7eb913
Altanyoza <altanyoza@gmail.com>
2025-04-02 10:37:37 +0900

    Add EmployeeController

---
+++ DVD-rent/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVD_rent.Models;
+
+namespace DVD_rent.Controllers
+{
+    class EmployeeController
+    {
+        //Id, Position, Login, Password, FullName
+        public static void AddEmployee(Position position, string login, string password)
+        {
+            using (Context db = new Context())
+            {
+                db.Employees.Add(new Employee { Position = position, Login = login, Password = password });
+                db.SaveChanges();
+            }
+        }
+        public static void EditEmployee(int id, Position position, string login, string password)
+        {
+            try
+            {
+                Employee employee = GetEmployeeById(id);
+                employee.Position = position;
+                employee.Login = login;
+                employee.Password = password;
+
+                Context db = new Context();
+                //if ()
+                //{
+                //throw new Exception("incorrect price");
+                //}
+                db.Entry(employee).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Caught an exception: {ex.Message}");
+            }
+        }
+
+        public static Employee GetEmployeeById(int id)
+        {
+            using (Context db = new Context())
+            {
+                return db.Employees.Find(id);
+            }
+        }
+        public static void DeleteEmployeeById(int id)
+        {
+            using (Context db = new Context())
+            {
+                Employee employee = GetEmployeeById(id);
+                db.Employees.Attach(employee);
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+            }
+        }
+        public static List<Employee> GetAllEmployees()
+        {
+            using (Context db = new Context())
+            {
+                return db.Employees.ToList();
+            }
+        }
+    }
+}
