3d842ea6e7d8344ae900f0ccd69644b1e8dee640
Dmitriev Arkady <Darkadiy999@gmail.com>
2025-05-15 09:19:00 +0900

    add migration

---
+++ DVD-rent/Migrations/202505150016106_merge conflicts.cs
@@ -0,0 +1,20 @@
+﻿namespace DVD_rent.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mergeconflicts : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Employees", "Login", c => c.String());
+            AlterColumn("dbo.Employees", "Password", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Employees", "Password", c => c.String(maxLength: 100));
+            AlterColumn("dbo.Employees", "Login", c => c.String(maxLength: 100));
+        }
+    }
+}
